8d4cf558ae2bab2c573d58e680c98a09e9d0942f
laya07 <137384092+laya07@users.noreply.github.com>
2023-06-22 13:24:11 +0530

    Add files via upload

---
+++ jackpot.js
@@ -0,0 +1,26 @@
+function jackpotWindow()
+{
+    var num1=Math.floor(Math.random()*5)
+    var num2=Math.floor(Math.random()*5)
+    var num3=Math.floor(Math.random()*5)
+    var num4=Math.floor(Math.random()*5)
+    var res=document.getElementsByClassName("a")
+    res[0].value=(`${num1}`)
+    res[1].value=(`${num2}`)
+    res[2].value=(`${num3}`)
+    res[3].value=(`${num4}`)
+
+    if(num1==num2&&num1==num3&&num1==num4)
+    {
+        var image=document.getElementsByTagName("img")
+        image[0].style.display="block"
+        image[0].style.transitionDuration="5s"
+        
+    }
+    else{
+        res[0].value=(`${num1}`)
+        res[1].value=(`${num2}`)
+        res[2].value=(`${num3}`)
+        res[3].value=(`${num4}`)
+    }
+}
